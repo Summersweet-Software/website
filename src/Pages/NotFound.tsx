087b337101003958ed16446af4f0f63ef8ede6d1
Abby Austin <38941820+spidertyler2005@users.noreply.github.com>
2026-03-23 00:47:26 -0400

    rewrite with vite

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export function NotFound() {
+  return (
+    <div className="text-center">
+      <h1>Page Could Not Be Found! (404)</h1>
+      <i style={{ color: "red" }}>
+        If this is a mistake, please create a ticket on github
+      </i>
+    </div>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export function MainFooter() {
+  return (
+    <footer className="bg-secondary text-white mt-5 p-4 position-sticky bottom-0">
+      <Container>
+        <Row>
+          <Col md={4}>
+            <h5>About Us</h5>
+            <p>A Maryland based software development group</p>
+          </Col>
+          <Col md={4}>
+            <h5>Quick Links</h5>
+            <Row className="align-items-start text-white">
+              <Col>
+                <a href="https://github.com/Summersweet-Software/">Github</a>
+              </Col>
+              <Col>
+                <a href="https://discord.gg/CZ3Hfpzdep">discord</a>
+              </Col>
+            </Row>
+          </Col>
+          {/*<Col md={4}>
+                <h5>Contact</h5>
+                <p>Email: contact@summersweet.software</p>
+            </Col>*/}
+        </Row>
+      </Container>
+    </footer>
+  );
+}
