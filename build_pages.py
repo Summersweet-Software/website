2712347bdd691351d3818902fce550eb4f58fe3e
Abby Austin <38941820+spidertyler2005@users.noreply.github.com>
2026-03-23 03:16:52 -0400

    make script to build pages

---
+++ build_pages.py
@@ -0,0 +1,17 @@
+"""Build all pages for the final distribution"""
+
+ALL_PAGES = ["projects", "projects/compiler-toolkit", "about"]
+INDEX_FILE = "index.html"
+DIST = "dist/"
+
+
+import os
+import shutil
+
+for page in ALL_PAGES:
+    print(f"[ ] Creating page for: {page}")
+    os.makedirs(DIST + page)
+    shutil.copyfile(INDEX_FILE, DIST + f"{page}/index.html")
+    print(f"[X] Created page for: {page}")
+
+print("Finished Creating Pages")
